bbf5e48482157511d195fdb7c278b27f5c1844a0
Jonathan Baltaci <genzio775@gmail.com>
2023-07-30 01:10:18 +0200

    initial commit

---
+++ d4builderback/src/test/java/com/example/d4builder/D4builderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.d4builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D4builderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
